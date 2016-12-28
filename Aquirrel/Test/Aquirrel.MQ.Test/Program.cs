83ed6b5a064d11033b7c82d7e8513092cce65851
czd890 <czd890@live.cn>
2016-12-28 10:49:48 +0800

    mq测试。发布nuget脚本

---
+++ Aquirrel/Test/Aquirrel.MQ.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+namespace Aquirrel.Logger.File.Test
+{
+
+    public class Program
+    {
+        static Startup startup;
+        static IServiceProvider sp;
+        internal static int C = 10;
+        public static void Main(string[] args)
+        {
+
+
+            startup = new Startup(null);
+            sp = startup.ConfigureServices(new ServiceCollection());
+            Console.Read();
+
+        }
+    }
+}
