a3a58d8f748b3fe37100111a105715f5c319a9b1
Calvin-ewb-PC <czd890@live.cn>
2017-01-06 02:16:39 +0800

    1.集成到web api并测试。 2.请求增加跟踪id。并形成api调用树

--- Aquirrel/Aquirrel.ResetApi/ApiClient.cs
+++ Aquirrel/Aquirrel.ResetApi/ApiClient.cs
@@ -6,71 +6,127 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http.Headers;
+using Aquirrel.ResetApi.Internal;
 
 namespace Aquirrel.ResetApi
 {
     public class ApiClient
     {
-        public ApiClient(ILogger<ApiClient> logger)
+        ILogger<ApiClient> Logger { get; set; }
+        IRestApiResolveApiUrl ApiResolveService { get; set; }
+        public ApiClient(ILogger<ApiClient> logger, IRestApiResolveApiUrl apiResolveService)
         {
             this.Logger = logger;
+            this.ApiResolveService = apiResolveService;
         }
-        ILogger<ApiClient> Logger { get; set; }
-        public T Execute<T>(IRequestBase<IResponseBase<T>> request)
+        public async Task<T> ExecuteAsync<T>(IRequestBase<IResponseBase<T>> request)
         {
-            Execute<IResponseBase<T>, T>(request);
-            throw new NotImplementedException();
+            var res = await ExecuteAsync<IResponseBase<T>, T>(request);
+            if (res.IsSuccess)
+            {
+                return res.data;
+            }
+            throw new BusinessException($"{res.msg}.errorcode:{res.error}");
         }
-        public IRes Execute<IRes, IData>(IRequestBase<IRes> request) where IRes : class, IResponseBase<IData>
+        public async Task<IRes> ExecuteAsync<IRes, IData>(IRequestBase<IRes> request) where IRes : class, IResponseBase<IData>
         {
-            this.ExecuteInternal(request).ContinueWith(new Func<Task<HttpResponseMessage>, object>(pp => { return null; }));
+            var resType = typeof(IRes);
+            var obj = (IRes)Activator.CreateInstance(resType);
 
-            return this.ExecuteInternal(request).ContinueWith(resTask =>
-             {
-                 var resType = typeof(IRes);
-                 var obj = (IRes)Activator.CreateInstance(resType);
+            var read = await ReadAsync(request, obj);
+            if (read.Item1)
+            {
+                try
+                {
+                    obj = read.Item2.ToJson<IRes>();
+                }
+                catch (Exception ex)
+                {
+                    obj.error = ResponseErrorCode.ToJsonError;
+                    obj.msg = ex.Message;
+                    this.Logger.LogError(0, ex, "rpc content to json error.{0}", read.Item2);
+                }
+            }
+            return obj;
 
-                 if (resTask.IsCanceled)
-                 {
-                     obj.error = ResponseErrorCode.TaskCancel;
-                     obj.msg = "task is canceled";
-                     return obj;
-                 }
-                 if (resTask.IsFaulted)
-                 {
-                     obj.error = ResponseErrorCode.TaskFail;
-                     obj.msg = resTask.Exception.GetBaseException().Message;
-                     this.Logger.LogError(0, resTask.Exception, "请求rpc接口错误.{0}", request.ToJson());
-                     return obj;
-                 }
-                 else
-                 {
-                     var res = resTask.Result;
-                     if (res.StatusCode != System.Net.HttpStatusCode.OK)
-                     {
-                         obj.error = res.IsSuccessStatusCode
- 
-                         return obj;
-                     }
-                     return obj;
-                 }
-             });
-            return null;
         }
-        public IResponseBase Execute(IRequestBase<IResponseBase> request)
+        public async Task<IResponseBase> ExecuteAsync(IRequestBase<IResponseBase> request)
         {
-            throw new NotImplementedException();
+            var obj = new ResponseBase();
+            var read = await this.ReadAsync(request, obj);
+            if (read.Item1)
+            {
+                try
+                {
+                    obj = read.Item2.ToJson<ResponseBase>();
+                }
+                catch (Exception ex)
+                {
+                    obj.error = ResponseErrorCode.ToJsonError;
+                    obj.msg = ex.Message;
+                    this.Logger.LogError(0, ex, "rpc content to json error.{0}", read.Item2);
+                }
+            }
+            return obj;
+        }
+
+        async Task<Tuple<bool, string>> ReadAsync(IRequest request, IResponseBase resObj)
+        {
+            var r = new Tuple<bool, string>(false, null);
+            var resTask = this.SendAsync(request);
+            var res = await resTask;
+            if (resTask.IsCanceled)
+            {
+                resObj.error = ResponseErrorCode.TaskCancel;
+                resObj.msg = "req rpc task is canceled";
+                return r;
+            }
+            if (resTask.IsFaulted)
+            {
+                resObj.error = ResponseErrorCode.TaskFail;
+                resObj.msg = resTask.Exception.GetBaseException().Message;
+                this.Logger.LogError(0, resTask.Exception, "req rpc api error.{0}", request.ToJson());
+                return r;
+            }
+
+            if (!res.IsSuccessStatusCode)
+            {
+                resObj.error = res.StatusCode.ToInt();
+                resObj.msg = res.ReasonPhrase;
+                return r;
+            }
+
+            var readTask = res.Content.ReadAsStringAsync();
+            var resultStr = await readTask;
+            if (readTask.IsFaulted)
+            {
+                resObj.error = ResponseErrorCode.ReadRpcContentError;
+                resObj.msg = readTask.Exception.GetBaseException().Message;
+                return r;
+            }
+            if (readTask.IsCanceled)
+            {
+                resObj.error = ResponseErrorCode.TaskCancel;
+                resObj.msg = "read prc content task is canceled";
+                return r;
+            }
+            return new Tuple<bool, string>(true, resultStr);
         }
 
         HttpClient httpClient = new HttpClient();
-        Task<HttpResponseMessage> ExecuteInternal(IRequest request)
+        Task<HttpResponseMessage> SendAsync(IRequest request)
         {
             var content = request.ToJson();
-            return httpClient.SendAsync(new HttpRequestMessage()
+            var req = new HttpRequestMessage()
             {
                 Method = request.Method,
-                Content = new StringContent(content, Encoding.UTF8, "application/json")
-            });
+                Content = new StringContent(content, Encoding.UTF8, "application/json"),
+                RequestUri = this.ApiResolveService.Resolve(request.App, request.ApiName)
+            };
+            req.Headers.Add(RestApiConst.TraceId, request.currentId);
+            return httpClient.SendAsync(req);
         }
     }
 }
