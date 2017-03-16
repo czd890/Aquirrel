ac96d6f65d67b2080880973264c1c30015e3029d
Calvin-ewb-PC <czd890@live.cn>
2017-03-17 02:10:21 +0800

    枚举获取显示内容

--- Aquirrel/Aquirrel/Extensions/EnumExtensions.cs
+++ Aquirrel/Aquirrel/Extensions/EnumExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,5 +19,22 @@ namespace Aquirrel
         {
             return (T)Enum.ToObject(typeof(T), source);
         }
+
+        public static string DisplayName(this Enum source)
+        {
+            if (source == null)
+                return null;
+            string description = source.ToString();
+            FieldInfo fieldInfo = source.GetType().GetTypeInfo().GetField(description);
+            if (fieldInfo == null)
+                return null;
+            DisplayAttribute[] attributes = (DisplayAttribute[])fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false);
+            if (attributes != null && attributes.Length > 0)
+            {
+                description = attributes[0].Name ?? attributes[0].Description;
+            }
+            return description;
+        }
+        enum xx { aa}
     }
 }
