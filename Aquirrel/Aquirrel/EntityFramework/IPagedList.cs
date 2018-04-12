e4585786ca4fa773534fc6898280e3edf498fd7f
czd890 <czd890@live.cn>
2018-04-12 15:12:12 +0800

    修改pagelist命名空间

--- Aquirrel/Aquirrel/EntityFramework/IPagedList.cs
+++ Aquirrel/Aquirrel/EntityFramework/IPagedList.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Aquirrel.EntityFramework
+namespace Aquirrel
 {
     /// <summary>
     /// Provides the interface(s) for paged list of any type.
