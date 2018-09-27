ec744f51ec939c1b872ee29a7a2d51d578f8a84e
czd890 <czd890@live.cn>
2018-09-27 18:55:21 +0800

    升级各类组件

--- Aquirrel/Aquirrel.MQ/Internal/CacheManager.cs
+++ Aquirrel/Aquirrel.MQ/Internal/CacheManager.cs
@@ -36,7 +36,7 @@ namespace Aquirrel.MQ.Internal
         }
         Dictionary<string, CacheItem> _cacheConn = new Dictionary<string, CacheItem>();
         EventBusSettings _settings;
-        ILogger<IEventBusInternal> _logger;
+        ILogger _logger;
         DateTime _lastCleanUpTime = DateTime.Now;
         public IConnection GetConnection(string productId)
         {
@@ -109,7 +109,7 @@ namespace Aquirrel.MQ.Internal
                     _cacheConn[productId] = new CacheItem();
                 }
 
-            Lable_GetModel:
+                Lable_GetModel:
                 bool hasGet = false;
                 IModel _channel = null;
                 foreach (var item in _cacheConn[productId].ConnItems)
